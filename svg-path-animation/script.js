ff7ef8494ef4d5c69b859f9d8bfcc31abce5c38d
N1Coder <tomofanteng123@gmail.com>
2024-06-12 19:36:28 +0800

    feat: learn advanced animation control using gsap setter getter and animate through motionpath svg gsap plugin

---
+++ svg-path-animation/script.js
@@ -0,0 +1,59 @@
+const select = (el) => document.querySelector(el)
+const selectAll = (el) => document.querySelectorAll(el)
+
+const home = select("#home")
+const candyStore = select("#candy")
+const dogPark = select("#dogpark")
+const school = select("#school")
+
+const pause = select("#pause")
+const progressSlider = select("#progressSlider")
+const time = select("#time")
+
+const animationOnUpdate = function () {
+  progressSlider.value = this.progress()
+  time.textContent = this.progress().toFixed(2)
+
+  progressSlider.addEventListener("input", function () {
+    animation.progress(this.value).pause()
+  })
+
+  progressSlider.addEventListener("change", function () {
+    pause.textContent = "Play"
+  })
+}
+
+const animation = gsap.to("#herman", {
+  duration: 5,
+  ease: "linear",
+  motionPath: {
+    path: "#motionpath",
+    align: "herman",
+  },
+  onUpdate: animationOnUpdate,
+  onComplete: () => (pause.textContent = "Play"),
+})
+
+pause.addEventListener("click", () => {
+  animation.paused(!animation.paused())
+
+  if (animation.progress() === 1) {
+    animation.restart()
+  }
+
+  pause.textContent = animation.paused() ? "Play" : "Pause"
+})
+
+const animateTo = (target, progress) => {
+  target.addEventListener("click", () => {
+    animation.pause()
+    pause.textContent = animation.paused() ? "Play" : "Pause"
+
+    gsap.to(animation, { progress: progress })
+  })
+}
+
+animateTo(home, 0)
+animateTo(candyStore, 0.5)
+animateTo(dogPark, 0.9)
+animateTo(school, 1)
